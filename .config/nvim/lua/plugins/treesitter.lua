bc23526cce2f85200266c42b59a9149f2023b2bd
Razushi <Razushi.SCVE@proton.me>
2025-10-16 15:25:07 +1100

    Update(Module): Nvim Config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,27 +1,23 @@
-return {
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  },
+return
+{
+    -- NOTE: Sets up and configures treesitter stuff
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed  = { "c", "lua", "vim", "vimdoc", "query", "rust", "python" },
+            -- auto_install = true,
+            highlight = { enable = true },
+
+            -- Keybinds using treesitter selections
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<Leader>ss", -- Set to 'false' to disable
+                    node_incremental = "<Leader>sa",
+                    scope_incremental = "<Leader>sc",
+                    node_decremental = "<Leader>sx",
+                },
+            },
+        })
+    end,
 }
